3f954cc955e65af53ba58d27acbca646550fea80
Tayen15 <tiantiantitik@gmail.com>
2025-08-13 19:28:53 +0700

    Refactor Hero section layout for improved responsiveness and positioning of decorative elements

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -18,50 +18,50 @@ export default function Hero() {
 
                {/* Decorative Background Elements - Radar + Univ */}
                <div className="absolute inset-0 pointer-events-none">
-                    {/* Radar SVG - Center, 70% width, higher z-index */}
-                    <div className="absolute left-1/2 top-0 transform -translate-x-1/2 translate-y-20 w-[60%] z-[-2]">
-                         <Image
-                              src="/assets/decorative/radar.png"
-                              alt="Radar"
-                              width={1000}
-                              height={1000}
-                              className="w-full h-auto opacity-100"
-                         />
+                    {/* Radar SVG - Center, responsive width and positioning */}
+                    <div className="absolute left-1/2 top-0 transform -translate-x-1/2 translate-y-70 md:translate-y-52 lg:translate-y-26 w-[80%] md:w-[70%] lg:w-[60%] z-[-2]">
+                          <Image
+                                src="/assets/decorative/radar.png"
+                                alt="Radar"
+                                width={1000}
+                                height={1000}
+                                className="w-full h-auto opacity-100"
+                          />
                     </div>
 
                     {/* Clouds - positioned and rotated as in the design */}
                     {/* Top Left Cloud */}
-                    <div className="absolute top-[28%] left-[30%] w-[80px] md:w-[100px] lg:w-[110px] z-[4] opacity-90">
-                         <Image src="/assets/cloud-right.png" alt="cloud" width={220} height={80} className="w-full h-auto" />
+                    <div className="absolute top-[30%] md:top-[28%] lg:top-[28%] left-[15%] md:left-[25%] lg:left-[30%] w-[60px] md:w-[80px] lg:w-[110px] z-[4] opacity-90">
+                          <Image src="/assets/cloud-right.png" alt="cloud" width={220} height={80} className="w-full h-auto" />
                     </div>
                     {/* Top Right Cloud */}
-                    <div className="absolute top-[16%] right-[29%] w-[70px] md:w-[100px] lg:w-[130px] z-[4] opacity-90">
-                         <Image src="/assets/cloud.png" alt="cloud" width={200} height={80} className="w-full h-auto" />
+                    <div className="absolute top-[18%] md:top-[16%] lg:top-[16%] right-[15%] md:right-[25%] lg:right-[29%] w-[50px] md:w-[80px] lg:w-[130px] z-[4] opacity-90">
+                          <Image src="/assets/cloud.png" alt="cloud" width={200} height={80} className="w-full h-auto" />
                     </div>
                     {/* Center Left Cloud */}
-                    <div className="absolute top-[45%] -left-[5%] w-[120px] md:w-[200px] lg:w-[350px] z-[2] -rotate-12 opacity-90">
-                         <Image src="/assets/cloud.png" alt="cloud" width={170} height={70} className="w-full h-auto" />
+                    <div className="absolute top-[50%] md:top-[45%] lg:top-[45%] -left-[10%] md:-left-[8%] lg:-left-[5%] w-[80px] md:w-[150px] lg:w-[350px] z-[2] -rotate-6 md:-rotate-8 lg:-rotate-12 opacity-90">
+                          <Image src="/assets/cloud.png" alt="cloud" width={170} height={70} className="w-full h-auto" />
                     </div>
                     {/* Center Right Cloud */}
-                    <div className="absolute top-[50%] -right-[7%] w-[120px] md:w-[200px] lg:w-[350px] z-[2] -rotate-12 opacity-100">
-                         <Image src="/assets/cloud.png" alt="cloud" width={160} height={70} className="w-full h-auto" />
+                    <div className="absolute top-[55%] md:top-[50%] lg:top-[50%] -right-[12%] md:-right-[10%] lg:-right-[7%] w-[80px] md:w-[150px] lg:w-[350px] z-[2] -rotate-6 md:-rotate-8 lg:-rotate-12 opacity-100">
+                          <Image src="/assets/cloud.png" alt="cloud" width={160} height={70} className="w-full h-auto" />
                     </div>
 
-                    <div className="absolute top-[15%] -left-[9%] w-[120px] md:w-[200px] lg:w-[350px] z-[1] -rotate-12 opacity-100">
-                         <Image src="/assets/hexagonal.png" alt="decorative" width={160} height={70} className="w-full h-auto" />
+                    {/* Hexagonal Left */}
+                    <div className="absolute top-[60%] md:top-[50%] lg:top-[15%] -left-[5%] md:-left-[12%] lg:-left-[9%] w-[80px] md:w-[150px] lg:w-[350px] z-[1] -rotate-6 md:-rotate-8 lg:-rotate-12 opacity-100">
+                            <Image src="/assets/hexagonal.png" alt="decorative" width={160} height={70} className="w-full h-auto" />
                     </div>
 
-                    <div className="absolute top-[15%] -right-[9%] w-[120px] md:w-[200px] lg:w-[350px] z-[0] -rotate-12 opacity-100">
-                         <Image src="/assets/hexagonal.png" alt="decorative" width={160} height={70} className="w-full h-auto" />
+                    {/* Hexagonal Right */}
+                    <div className="absolute top-[60%] md:top-[50%] lg:top-[15%] -right-[5%] md:-right-[12%] lg:-right-[9%] w-[80px] md:w-[150px] lg:w-[350px] z-[0] -rotate-6 md:-rotate-8 lg:-rotate-12 opacity-100">
+                            <Image src="/assets/hexagonal.png" alt="decorative" width={160} height={70} className="w-full h-auto" />
                     </div>
 
                     {/* University SVG - Positioned behind countdown area */}
                     <div className="absolute bottom-0 left-0 right-0 flex justify-center items-end h-[70%]">
-
-                         <div className="flex justify-center items-end ">
-
-                              {/* Left Building */}
-                              <div className="absolute right-[calc(100%-970px)] w-full h-full z-[1]">
+                         <div className="flex justify-center items-end">
+                              {/* Left Building - Hidden on mobile, visible on tablet+ */}
+                              <div className="hidden lg:block absolute -left-[45%] lg:-left-[36%] md:-left-[35%] w-full h-full z-[1]">
                                    <Image
                                         src="/assets/background/building/left.png"
                                         alt=""
@@ -71,19 +71,19 @@ export default function Hero() {
                                    />
                               </div>
 
-                              {/* Center Building - Main university */}
-                              <div className="absolute bottom-5 w-full h-full z-[0]">
+                              {/* Center Building - Always visible, responsive sizing */}
+                              <div className="absolute bottom-5 w-full h-full z-[0] md:left-0 md:right-0 md:w-full lg:left-0 lg:right-0 lg:w-full">
                                    <Image
                                         src="/assets/background/building/center.png"
                                         alt=""
                                         width={680}
                                         height={600}
-                                        className="w-full h-full object-contain object-bottom opacity-100"
+                                        className="w-[130%] md:w-full lg:w-full h-full object-contain object-bottom opacity-100"
                                    />
                               </div>
 
-                              {/* Right Building */}
-                              <div className="absolute left-[calc(100%-950px)] w-full h-full">
+                              {/* Right Building - Hidden on mobile, visible on tablet+ */}
+                              <div className="hidden lg:block absolute -right-[45%] lg:-right-[36%] md:-right-[35%] w-full h-full z-[1]">
                                    <Image
                                         src="/assets/background/building/right.png"
                                         alt=""
@@ -94,9 +94,8 @@ export default function Hero() {
                               </div>
                          </div>
 
+                         {/* Shadow Overlay - Always visible, responsive */}
                          <div className="flex flex-col-reverse justify-center items-end">
-
-                              {/* Shadow Overlay - Gradient from bottom to top */}
                               <div className="absolute bottom-0 left-0 right-0 h-[150%] z-[2] pointer-events-none"
                                    style={{
                                         background: 'linear-gradient(to top, rgba(226,220,0,0.4) 0%, rgba(226,220,0,0.3) 20%, rgba(226,220,0,0.2) 30%, rgba(226,220,0,0.1) 40%, rgba(226,220,0,0.05) 50%, transparent 60%)'
@@ -104,7 +103,8 @@ export default function Hero() {
                               </div>
                          </div>
 
-                         <div className="absolute -bottom-24 rotate-y-180 flex justify-center items-end h-[60%] w-[85%] z-[1]">
+                         {/* Road - Always visible, responsive sizing and positioning */}
+                         <div className="absolute -bottom-24 md:-bottom-24 lg:-bottom-24 rotate-y-180 flex justify-center items-end h-[60%] w-[85%] md:w-[85%] lg:w-[85%] z-[1]">
                               <Image
                                    src="/assets/background/building/road.png"
                                    alt=""
@@ -117,10 +117,10 @@ export default function Hero() {
                </div>
 
                {/* Content */}
-               <div className="relative z-content max-w-container mx-auto px-4 py-6 z-[3]">
+               <div className="relative z-content max-w-container mx-auto px-4 py-4 lg:py-6 z-[3]">
                     <div className="flex flex-col items-center">
                          {/* Logo */}
-                         <div className="mb-3">
+                         <div className="mb-2 lg:mb-3">
                               <Image
                                    src="/assets/logo-ormik.svg"
                                    alt="ORMIK 2025"
@@ -131,7 +131,7 @@ export default function Hero() {
                          </div>
 
                          {/* Headline with Multiple Ready to Explore Assets */}
-                         <div className="relative mb-2 flex items-center justify-center">
+                         <div className="relative mb-1 lg:mb-2 flex items-center justify-center">
                               {/* Layered Ready to Explore Assets */}
                               <div className="relative">
                                    {/* Base Layer - Main Asset with Drop Shadow */}
@@ -170,7 +170,7 @@ export default function Hero() {
                          </div>
 
                          {/* CTA Button */}
-                         <div className="relative mb-36">
+                         <div className="relative mb-8 lg:mb-20">
                               <button className="w-44 h-11 bg-blue-950 rounded-[8px] flex items-center justify-center cursor-pointer select-none
                                    hover:translate-y-2 hover:[box-shadow:0_0px_0_0_rgba(226,220,0,1.00),0_0px_0_0_rgba(226,220,0,0.4)]
                                    hover:border-b-[0px]
@@ -195,7 +195,7 @@ export default function Hero() {
                          </div>
 
                          {/* Countdown Component */}
-                         <div className="mb-4 lg:mb-8">
+                         <div className="mb-3 lg:mb-6">
                               <Countdown />
                          </div>
 
