f3eda0255101dd78b91f6d2d1eda10c4ffff0dc2
Tayen15 <tiantiantitik@gmail.com>
2025-08-20 13:29:35 +0700

    feat: add AdminRobotsPage with RobotsManager component

---
+++ src/app/admin/robots/page.tsx
@@ -0,0 +1,7 @@
+import RobotsManager from '@/components/admin/RobotsManager';
+
+export const dynamic = 'force-dynamic';
+
+export default function AdminRobotsPage() {
+     return <RobotsManager />;
+}
